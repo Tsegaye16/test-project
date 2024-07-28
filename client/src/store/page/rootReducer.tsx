59f230de9c01201f333c5d15db3074e2ec8b3bca
Tsegaye16 <abewatsegaye16@gmail.com>
2024-07-28 23:43:32 +0300

    Refactored

--- client/src/store/page/rootReducer.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import songsReducer from "./slice";
-
-const rootReducer = combineReducers({
-  songs: songsReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
